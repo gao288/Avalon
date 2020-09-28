02da0ea91ca11122ee4df48f0b4d766600b26e24
Xufei Gao <gxf9625@gmail.com>
2020-09-28 00:05:20 -0700

    first init

---
+++ src/Game/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Welcome extends React.Component {
+    constructor(props){
+        super(props);
+        this.props = props;
+        // this.player = [];
+        // this.mode = null;
+    } 
+    render() {
+        return(
+            <div> </div>
+        )
+    }
+    
+}
+
+export default Welcome;
